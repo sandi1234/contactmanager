e7ebc86ea8d321b416c2409f58ebd91da0d2538e
sandi1234 <samsonndizawa@hotmail.com>
2019-03-04 19:35:51 +0100

    Updates

---
+++ precache-manifest.e3b8467ddf9878e9e2d3d5e8bfb2fd23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "6ee31e8897db9687bef8",
+    "url": "/contactmanager/static/js/main.6ee31e88.chunk.js"
+  },
+  {
+    "revision": "14c01ddde46dc1ee142d",
+    "url": "/contactmanager/static/js/2.14c01ddd.chunk.js"
+  },
+  {
+    "revision": "6ee31e8897db9687bef8",
+    "url": "/contactmanager/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "14c01ddde46dc1ee142d",
+    "url": "/contactmanager/static/css/2.5d569e03.chunk.css"
+  },
+  {
+    "revision": "eeb76f42b3b59ec503110db5f7a76e81",
+    "url": "/contactmanager/index.html"
+  }
+];
